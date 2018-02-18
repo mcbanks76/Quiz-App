ef4a4821e522a7da2382dd4c12ba3d19aae82fb6
Matt Banks <mattkbanks@gmail.com>
2018-02-18 14:20:29 -0500

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,85 @@
+'use strict'
+let quizQuestion = questions([
+	{
+	"question": "What 80’s TV show, starring Scott Bakula, is one of the greatest shows to ever grace the airways? (For like, a million reasons. Don’t get me started...)",
+	"choiceA": "The A-Team",
+	"choiceB": "MacGyver",
+	"choiceC": "Quantum Leap",
+	"choiceD": "The Golden Girls",
+	"correct": "C"
+	},
+	{
+	"question": "Who was the main villain on the cartoon “He-Man and the Masters of the Universe?",
+	"choiceA": "Rochester",
+	"choiceB": "Skeletor",
+	"choiceC": "Man-at-Arms",
+	"choiceD": "She-Ra",
+	"correct": "B"
+	},
+	{
+	"question": "In what year did “The Simpsons” first air, starting a reign of televised comedic dominance that will not be replicated in your lifetime?",
+	"choiceA": "1987",
+	"choiceB": "1983",
+	"choiceC": "1989",
+	"choiceD": "Nineteen aught four",
+	correct: "C"
+	},
+	{
+	"question": "On 'Knight Rider', what was the name of the car that Michael drove around, fighting crime and the like?",
+	"choiceA": "ROC",
+	"choiceB": "TRIPP",
+	"choiceC": "KITT",
+	"choiceD": "Samantha",
+	"correct": "C"
+	},
+	{
+	"question": "On “The Golden Girls”, who owned the house they all lived in?",
+	"choiceA": "Blanche",
+	"choiceB": "Dorothy",
+	"choiceC": "Rose",
+	"choiceD": "Sophia",
+	"correct": "A"
+	},
+	{
+	"question?": "What was the name of the robot on the surprisingly watchable show 'Small Wonder'?",
+	"choiceA": "Pam",
+	"choiceB": "Reba",
+	"choiceC": "Vicky",
+	"choiceD": "Max",
+	"correct": "C"
+	},
+	{
+	"question": "Which Saturday morning cartoon series featured tiny versions of iconic children's characters?",
+	"choiceA": "Howdy Doody Tots",
+	"choiceB": "Muppet Babies",
+	"choiceC": "He-Man: The Nursery Years",
+ 	"choiceD": "Ultrasound Voltron",
+ 	"correct": "B"
+	},
+
+	"question": "What planet did ALF come from?",
+ 	"choiceA": "Marvin",
+ 	"choiceB": "Crapo",
+  	"choiceC": "Remulac",
+  	"choiceD": "Melmac",
+  	"correct": "D"
+   	},
+
+	"question": "Which show was a spinoff of 'Growing Pains'?",
+	"choiceA": "Boner and the Deuce",
+ 	"choiceB": "Mike Seaver High",
+ 	"choiceC": "Just the ten of us",
+ 	"choiceD": "Out of this world",
+	"correct": "D"
+	},
+
+	"question": "What was catchphrase of John 'Hanibal' Smith (From the TV show The A-Team. Although, if you need this clue, I worry you ain't gettin' this right)?",
+	"choiceA": "Do you smell something?",
+	"choiceB": "I love it when a plan comes together.",
+  	"choiceC": "This pup's gotta fly!",
+ 	"choiceD": "A-Team...Let's Finish the Job.",
+	"correct": "B"
+	}
+];
+
+
